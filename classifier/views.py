fb603bb07c7f180441c49de270eb6c129d83d18f
XOR083199 <sorianodanielb99@gmail.com>
2020-05-18 11:43:56 +0800

    Added Binary Model

--- classifier/views.py
+++ classifier/views.py
@@ -9,6 +9,7 @@ from PIL import Image
 LABELS = ['Bacterial Blight', 'Septorial Brown Spot', 'Frogeye Leaf Spot', 'Healthy', 'Herbicide Injury', 
           'Iron Deficiency Chlorosis', 'Potassium Deficiency', 'Bacterial Pustule', 'Sudden Death Syndrome']
 
+model_binary = tf.keras.models.load_model("./classifier/leaf_final.h5")
 model = tf.keras.models.load_model('./classifier/model_Monday-09-02-2019-17-36-12')
 model_features_extractor = tf.keras.models.Model(inputs=model.input, outputs=model.get_layer("GlobalAveragePooling2D_1").output)
 svm_model = joblib.load('./classifier/svm_rbf_2020-04-21 17_08_26.741869')
@@ -23,11 +24,20 @@ def predict(request):
     img = tf.keras.preprocessing.image.load_img(img_path, target_size=(64, 64))
     img = tf.keras.preprocessing.image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
-    img_features = model_features_extractor.predict(img)
-    result = svm_model.predict(img_features)
+    detection = model_binary.predict(img)
+    detection = np.argmax(detection, axis=1)
+    if detection == 1:
+        img_features = model_features_extractor.predict(img)
+        result = svm_model.predict(img_features)
 
-    data = {
-        'result': str(LABELS[result[0]])
-    }
+        data = {
+            'leaf': True,
+            'class': str(LABELS[result[0]])
+        }
+    else:
+        data = {
+            'leaf': False,
+            'class': None
+        }
     
     return JsonResponse(data)
