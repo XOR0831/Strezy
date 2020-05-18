c6fde41b3662098c96d4b697e71f756b3e11008a
XOR083199 <sorianodanielb99@gmail.com>
2020-05-18 14:02:12 +0800

    Fix Logger

--- classifier/views.py
+++ classifier/views.py
@@ -25,7 +25,6 @@ def predict(request):
     img = tf.keras.preprocessing.image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
     detection = model_binary.predict(img)
-    detection = np.argmax(detection, axis=1)[0]
     print(detection)
     if detection == 1:
         img_features = model_features_extractor.predict(img)
