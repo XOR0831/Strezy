b197ccd5b1fcb78b1e57be751ed46716f3c4deb6
XOR083199 <sorianodanielb99@gmail.com>
2020-05-19 16:31:06 +0800

    Added Migrations

--- classifier/models.py
+++ classifier/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class History(models.Model):
     title = models.CharField(max_length=255)
-    datetime = models.DateTimeField(auto_now=Ture)
+    datetime = models.DateTimeField(auto_now=True)
